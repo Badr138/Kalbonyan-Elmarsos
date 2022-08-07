34adff9b319d4b9669da6125399fb669190e0e54
Badr H <Business132784@gmail.com>
2022-08-07 20:19:37 +0200

    the final 02-Fundamentals-Part-2

--- 02-Udemy/-02-Js-Jonas/Code/02-Fundamentals-Part-2/practice-assignments.js
+++ 02-Udemy/-02-Js-Jonas/Code/02-Fundamentals-Part-2/practice-assignments.js
@@ -28,3 +28,231 @@ console.log(percPortugal1, percChina1, percUSA1);
 */
 
 /////  Arrow Functions /////
+/*
+// function declartions
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+// function expressions
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+}
+
+// Arrow function
+const percentageOfWorld3 = population => (population / 7900) * 100;
+
+const percPortugal3 = percentageOfWorld3(10);
+const percChina3 = percentageOfWorld3(1441);
+const percUSA3 = percentageOfWorld3(332);
+
+const percPortugal1 = percentageOfWorld1(10);
+const percChina1 = percentageOfWorld1(1441);
+const percUSA1 = percentageOfWorld1(332);
+console.log(percPortugal1, percChina1, percUSA1, percPortugal3, percChina3, percUSA3);
+*/
+
+///// Functions Calling Other Functions /////
+/*
+const describePopulation = function (country, population) {
+  const percentage = percentageOfWorld1(population);
+  const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+  console.log(description);
+};
+const percentageOfWorld1 = (pop) => (pop / 7900) * 100;
+
+describePopulation('Portugal', 10);
+describePopulation('China', 1441);
+describePopulation('USA', 332);
+*/
+///// Introduction to Arrays /////
+
+/*
+const populations = [12, 190, 67, 45];
+console.log(populations.length === 4);
+
+const percentages = [
+  percentageOfWorld1 = (populations[0]),
+  percentageOfWorld1 = (populations[1]),
+  percentageOfWorld1 = (populations[2]),
+  percentageOfWorld1 = (populations[3])
+]
+console.log(percentages);
+*/
+
+///// Basic Array Operations (Methods) /////
+/*
+const neighbours = ['Norway', 'Sweden', 'Russia'];
+
+neighbours.push('Utopia');
+console.log(neighbours);
+
+neighbours.pop();
+console.log(neighbours);
+
+if (neighbours.includes('Germany')) {
+} else {
+  console.log('Probably not a central European country :D');
+} if (neighbours.indexOf('Sweden')) {
+  neighbours[1] = 'Republic of Sweden';
+}
+console.log(neighbours);
+/*
+
+// Another solution
+/*
+const neighbours = ['Norway', 'Sweden', 'Russia'];
+
+neighbours.push('Utopia');
+console.log(neighbours);
+
+neighbours.pop();
+console.log(neighbours);
+
+if (!neighbours.includes('Germany')) {
+console.log('Probably not a central European country :D');
+}
+
+neighbours[neighbours.indexOf('Sweden')] = 'Republic of Sweden';
+console.log(neighbours);
+*/
+
+///// Introduction to Objects /////
+/*
+const myCountry = {
+  country: 'Egypt',
+  capital: 'Cairo',
+  language: 'Arabic',
+  population: 102,
+  neighbours: ['Norway', 'Sweden', 'Russia']
+}
+console.log(myCountry);
+*/
+
+///// Dot vs. Bracket Notation /////
+/*
+// 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki.'
+
+const myCountry = {
+  country: 'Egypt',
+  capital: 'Cairo',
+  language: 'Arabic',
+  population: 102,
+  neighbours: ['Norway', 'Sweden', 'Russia']
+}
+
+myCountry.population += 2;
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`
+);
+
+myCountry['population'] -= 2;
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`
+);
+*/
+
+///// Object Methods /////
+/*
+const myCountry = {
+  country: 'Egypt',
+  capital: 'Cairo',
+  language: 'Arabic',
+  population: 102,
+  neighbours: ['Norway', 'Sweden', 'Russia'],
+
+  describe: function () {
+    console.log(
+      `${this.country} has ${this.population} million 
+      ${this.language}-speaking people, 
+      ${this.neighbours.length} neighbouring countries and a 
+      capital called ${this.capital}.`
+    );
+  },
+
+  checkIsland: function() {
+    this.isIsland = this.neighbours.length === 0 ? true : false;
+
+    // Even simpler version (see why this works...)
+    // this.isIsland = !Boolean(this.neighbours.length);
+  }
+}
+myCountry.describe();
+myCountry.describe();
+console.log(myCountry);
+*/
+
+///// Iteration: The for Loop /////
+
+/*
+for (let voter = 1; voter <=50 ;voter++) {
+  console.log(`Voter number ${voter} is currently voting`);
+}
+*/
+
+///// Looping Arrays, Breaking and Continuing /////
+/*
+// const populations = [12, 190, 67, 45];
+const populations = [10, 1441, 332, 83];
+console.log(populations.length === 4);
+
+const percentages = [
+  percentageOfWorld1 = (populations[0]),
+  percentageOfWorld1 = (populations[1]),
+  percentageOfWorld1 = (populations[2]),
+  percentageOfWorld1 = (populations[3])
+]
+console.log(percentages);
+
+const percentages2 = [];
+for (let i = 0; i > populations.length; i++) {
+  const perc = percentageOfWorld1(populations[i]);
+  percentages2.push(perc);
+}
+console.log(percentages2);
+*/
+
+///// Looping Backwards and Loops in Loops /////
+/*
+const listOfNeighbours = [
+  ['Canada', 'Mexico'], 
+  ['Spain'], 
+  ['Norway', 'Sweden', 'Russia']
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++)
+  for (let y = 0; y < listOfNeighbours[i].length; y++)
+    console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
+*/
+
+///// The while Loop /////
+/*
+// const populations = [12, 190, 67, 45];
+const populations = [10, 1441, 332, 83];
+console.log(populations.length === 4);
+const percentageOfWorld1 = (pop) => (pop / 7900) * 100;
+
+// const percentages = [
+//   percentageOfWorld1 = (populations[0]),
+//   percentageOfWorld1 = (populations[1]),
+//   percentageOfWorld1 = (populations[2]),
+//   percentageOfWorld1 = (populations[3])
+// ]
+// console.log(percentages);
+
+const percentages2 = [];
+for (let i = 0; i > populations.length; i++) {
+  const perc = percentageOfWorld1(populations[i]);
+  percentages2.push(perc);
+}
+console.log(percentages2);
+
+const percentages3 = [];
+ let i = 0;
+ while (i < populations.length) {
+ const perc = percentageOfWorld1(populations[i]);
+ percentages3.push(perc);
+ i++;
+ }
+ console.log(percentages3)
+*/
--- 02-Udemy/-02-Js-Jonas/Code/02-Fundamentals-Part-2/script.js
+++ 02-Udemy/-02-Js-Jonas/Code/02-Fundamentals-Part-2/script.js
@@ -76,3 +76,347 @@ console.log(yearsUntilRetirement(1980, 'Bob'));
 
 ////////////////////////////////////
 // Functions Calling Other Functions
+/*
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+*/
+
+////////////////////////////////////
+// Reviewing Functions
+/*
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+  const age = calcAge(birthYeah);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired `);
+    return -1;
+  }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
+
+////////////////////////////////////
+// Introduction to Arrays
+/*
+const friend1 = 'Micheal';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Micheal', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length -1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice'];
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthday) {
+  return 2037 - birthday;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length -1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length -1])];
+console.log(ages);
+*/
+
+////////////////////////////////////
+// Basic Array Operations (Methods)
+
+/*
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+// ES6
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friends called Steven');
+}
+*/
+
+////////////////////////////////////
+//  Introduction to Objects
+/*
+const jonasArray = [
+  'jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+  firstName: 'jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+*/
+
+////////////////////////////////////
+//  Dot vs. Bracket Notation
+
+/*
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+}
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// console.log(jonas.'last' + nameKey);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, LastName, age, job, and friends');
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+jonas.location = 'Portugal';
+jonas['Twitter'] = '@joansschmedtman';
+console.log(jonas);
+
+// challenge
+// "jonas has 3 friends, and his best friends is called Michael"
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friends is called ${jonas.friends[0]}`);
+*/
+
+////////////////////////////////////
+// Object Methods
+
+/*
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYeah: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYeah) {
+  //   return 2037 - birthYeah;
+  // }
+
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2037 - this.birthYeah;
+  // }
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYeah;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} drivers's license`;
+  }
+}
+
+console.log(jonas.calcAge());
+// console.log(jonas['calcAge']());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// Challenge
+// "Jonas is a 46-year old teacher, and he has a driver's license"
+console.log(jonas.getSummary());
+*/
+
+////////////////////////////////////
+// Iteration: The for Loop
+
+/*
+// console.log('Lifting weights reptition 1');
+// console.log('Lifting weights reptition 2');
+// console.log('Lifting weights reptition 3');
+// console.log('Lifting weights reptition 4');
+// console.log('Lifting weights reptition 5');
+// console.log('Lifting weights reptition 6');
+// console.log('Lifting weights reptition 7');
+// console.log('Lifting weights reptition 8');
+// console.log('Lifting weights reptition 9');
+// console.log('Lifting weights reptition 10');
+
+// for loop keps running while condition is TRUE
+for (let rep = 1; rep <= 30; rep++) {
+  console.log(`Lifting weights reptition ${rep}`);
+}
+*/
+
+////////////////////////////////////
+// Looping Arrays, Breaking and Continuing
+/*
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+const types = [];
+// console,log(jonas[0]);
+// console.log(jonas[1]);
+// ...
+// console.log(jonas[4]);
+// jonas[5] does NOT exist
+
+for (let i = 0; i < jonas.length; i++) {
+  // Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+
+  // Filling types array
+  // types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// Continue and braek
+console.log('--- ONLY STRINGS ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== 'string') continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('--- BREAK WITH NUMBER ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === 'number') break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+*/
+
+////////////////////////////////////
+// Looping Backwards and Loops in Loops
+/*
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for ( let exercise = 1; exercise < 4; exercise++) {
+  console.log(`--------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
+*/
+
+////////////////////////////////////
+// The while Loop
+/*
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
+}
+*/
